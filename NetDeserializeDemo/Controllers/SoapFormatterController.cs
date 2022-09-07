fb2a3a3faeaeb33febf84b938b6dbac20eaee8f6
l1k3beef <l1k3beef@outlook.com>
2022-09-07 11:34:32 +0800

    SoapFormatter Deserialize Vulnerability

---
+++ NetDeserializeDemo/Controllers/SoapFormatterController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace NetDeserializeDemo.Controllers
+{
+
+    public class SoapFormatterController : ApiController
+    {
+        // GET api/soapFormatter
+        public string Get()
+        {
+            return "SoapFormatter Deserialize Vulnerability";
+        }
+
+        // POST api/soapFormatter/id
+        public void Post(int id, [FromBody] string value)
+        {
+            switch (id)
+            {
+                case 1:
+                    DeserializeData(value);
+                    break;
+                case 2:
+                    UnsafeDeserialize(value);
+                    break;
+                case 3:
+                    DeserializeMethodResponse(value);
+                    break;
+                case 4:
+                    UnsafeDerializeMethodResponse(value);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public object DeserializeData(string data)
+        {        
+            MemoryStream memoryStream = new MemoryStream(System.Convert.FromBase64String(data));
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            return binaryFormatter.Deserialize(memoryStream);
+        }
+
+        public object UnsafeDeserialize(string data)
+        {
+            MemoryStream memoryStream = new MemoryStream(System.Convert.FromBase64String(data));
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            return binaryFormatter.UnsafeDeserialize(memoryStream, null);
+        }
+
+        public object DeserializeMethodResponse(string data)
+        {
+            MemoryStream memoryStream = new MemoryStream(System.Convert.FromBase64String(data));
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            return binaryFormatter.DeserializeMethodResponse(memoryStream, null, null);
+        }
+
+        public object UnsafeDerializeMethodResponse(string data)
+        {
+            MemoryStream memoryStream = new MemoryStream(System.Convert.FromBase64String(data));
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            return binaryFormatter.UnsafeDeserializeMethodResponse(memoryStream, null, null);
+        }
+
+
+    }
+}
